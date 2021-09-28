f8f7d552659e5fde66196e13636d1dae985f1dc5
byulyi kim <byulyikeem@gmail.com>
2021-09-29 02:41:35 +0900

    feat : 테이블 데이터 및 css 추가

--- js/api.js
+++ js/api.js
@@ -1,1 +1,7 @@
-"use strict";
+const API_URL = "../domino_web_exam.json";
+
+export const getStockList = () => {
+  const response = fetch(API_URL).then((res) => res.json());
+
+  return response;
+};
--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-"use strict";
+import { getStockList } from "./api.js";
+
+const initLoader = () => {
+  getStockList().then((res) => {
+    res.map((data) => {
+      const stockTable = document.querySelector(".table__stocks");
+      const stockItem = document.createElement("tr");
+      const title = document.createElement("td");
+      const price = document.createElement("td");
+
+      title.innerHTML = `${data.stock.name}`;
+      price.innerHTML = `${data.price}`;
+
+      stockItem.classList.add("stock__item");
+      stockItem.appendChild(title);
+      stockItem.appendChild(price);
+
+      stockTable.appendChild(stockItem);
+    });
+  });
+};
+
+window.addEventListener("DOMContentLoaded", () => initLoader());
