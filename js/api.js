16348a426eb5ad676cad40a0e9839deddc5ddde7
byulyi kim <byulyikeem@gmail.com>
2021-09-29 15:39:56 +0900

    feat : 가격에 따른 정렬 추가

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const API_URL = "../domino_web_exam.json";
 
 export const getStockList = () => {
-  const response = fetch(API_URL).then((res) => res.json());
+  const data = fetch(API_URL).then((res) => res.json());
 
-  return response;
+  return data;
 };
--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 import { getStockList } from "./api.js";
 
+const stockTable = document.querySelector(".table__stocks");
+const stockSortBtn = document.querySelector(".table__sortBtn");
+let sortState = "asc";
+
 const initLoader = () => {
-  getStockList().then((res) => {
-    res.map((data) => {
-      const stockTable = document.querySelector(".table__stocks");
+  getStockList().then((stockList) => {
+    stockList.map((data) => {
       const stockItem = document.createElement("tr");
       const title = document.createElement("td");
       const price = document.createElement("td");
@@ -12,6 +15,8 @@ const initLoader = () => {
       price.innerHTML = `${data.price}`;
 
       stockItem.classList.add("stock__item");
+      title.classList.add("stock__item__title");
+      price.classList.add("stock__item__price");
       stockItem.appendChild(title);
       stockItem.appendChild(price);
 
@@ -20,4 +25,30 @@ const initLoader = () => {
   });
 };
 
+const handleSortStocks = () => {
+  const rows = stockTable.getElementsByTagName("tr");
+  let runSort = true;
+  sortState = sortState === "asc" ? "desc" : "asc";
+
+  while (runSort) {
+    runSort = false;
+
+    for (let i = 0; i < rows.length - 1; i++) {
+      const price = Number(rows[i].getElementsByTagName("td")[1].innerText);
+      const nextPrice = Number(
+        rows[i + 1].getElementsByTagName("td")[1].innerText
+      );
+
+      if (
+        (sortState === "asc" && price > nextPrice) ||
+        (sortState === "desc" && price < nextPrice)
+      ) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        runSort = true;
+      }
+    }
+  }
+};
+
+stockSortBtn.addEventListener("click", handleSortStocks);
 window.addEventListener("DOMContentLoaded", () => initLoader());
