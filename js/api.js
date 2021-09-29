ae9717a1ef64ed81619c11bd45f50e3e6ec05327
byulyi kim <byulyikeem@gmail.com>
2021-09-29 15:49:24 +0900

    update : json -> 깃헙 업로드 주소로 수정

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
-const API_URL = "../domino_web_exam.json";
+const API_URL =
+  "https://raw.githubusercontent.com/kimbyulyi/pretask-front-ffwd/main/domino_web_exam.json";
 
 export const getStockList = () => {
   const data = fetch(API_URL).then((res) => res.json());
